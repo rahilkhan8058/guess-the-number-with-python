b50aba2f3f907dfc8be4f747b4384e3a9532bbd1
rahilkhan8058 <78581875+rahilkhan8058@users.noreply.github.com>
2025-03-06 12:20:30 +0530

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import random
 n=random.randint(1,100)
 a=-1
-guesses = 0
+guesses = 1
 while (a!=n):
-    guesses +=1
     a=int(input("Guess the number : "))
     if (a>n):
         print("LOWER NUMBER PLEASE")
-    else:
+        guesses +=1                 
+
+    elif (a<n):
         print("HEIGHER NUMBER PLEASE")
-        
-print(f"Congratulations !! you have guessed the number correct {guesses} attempt")
+        guesses +=1                 
+
+print(f"Congratulations !! you have guessed the number {n} correctly in  {guesses} attempt")
+
